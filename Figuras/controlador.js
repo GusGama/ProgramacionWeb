235f70c2b104558aad86785280c135fd10d4fef3
Unknown <ryuuji6711@gmail.com>
2018-05-07 13:36:31 -0500

    funcion

--- Figuras/controlador.js
+++ Figuras/controlador.js
@@ -3,3 +3,16 @@ function calcular(){
 	var num2 = document.getElementById("num2").value;
 	alert(num2 +num1);
 }
+
+function Cilindro(radio, altura) 
+	{	alert("El volumen del Cilindro es: "+(3.1416*radio*radio)*altura);	}
+
+function Cono(radio, altura) 
+	{	alert("El volumen del Cilindro es: "+((3.1416*radio*radio)*altura))/3;	}
+
+function Cubo(lado) 
+	{	alert("El volumen del Cubo es: "+(lado*lado*lado);	}
+
+function Esfera(radio) 
+	{	alert("El volumen de la Esfera es: "+(4*3.1416*(radio*radio))/3;	}
+
